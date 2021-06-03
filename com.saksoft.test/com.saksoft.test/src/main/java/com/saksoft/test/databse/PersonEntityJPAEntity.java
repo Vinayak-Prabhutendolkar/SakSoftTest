d07c55429bc19c16782d61b9bcc8b6f53bb7f242
Vinayak Prabhutendolkar <74896504+Vinayak-Prabhutendolkar@users.noreply.github.com>
2021-06-03 23:29:50 +0530

    Delete com.saksoft.test/com.saksoft.test directory

--- com.saksoft.test/com.saksoft.test/src/main/java/com/saksoft/test/databse/PersonEntityJPAEntity.java
+++
@@ -1,7 +0,0 @@
-package com.saksoft.test.databse;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonEntityJPAEntity extends JpaRepository<PersonEntity, Integer> {
-
-}
